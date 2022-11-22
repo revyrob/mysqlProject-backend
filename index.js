e25b4d1ad2582552a249faf6a6afe0c888e7b9c1
revyrob <kayle.robson@gmail.com>
2022-11-21 16:49:41 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mysql from "mysql";
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "test", //we created the test database in the mysql workbench
+});
+
+//if there is an auth problem
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootroot';
+
+//we need a middleware so that we can send data through express
+//therefore we need a middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `description`, `cover`) VALUES (?) ";
+  const values = [req.body.title, req.body.description, req.body.cover];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created");
+  });
+});
+
+app.listen(8080, () => {
+  console.log("Connected to backend, port 8080!");
+});
